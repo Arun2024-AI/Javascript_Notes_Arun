d796d02c83276b91249c50049cf8b2099825a1df
Arun Kumar Giri <atulgiri649@gmail.com>
2025-03-20 23:42:01 +0530

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,12 +1,12 @@
-if(a==1){
-    console.log("a is equal to 1");
-} else if(a==2){
-    console.log("a is equal to 2");
-} else if(a==3){
-    console.log("a is equal to 3");
-} else{
-    console.log("a is equal to 1,2 or 3");
-}
+// if(a==1){
+//     console.log("a is equal to 1");
+// } else if(a==2){
+//     console.log("a is equal to 2");
+// } else if(a==3){
+//     console.log("a is equal to 3");
+// } else{
+//     console.log("a is equal to 1,2 or 3");
+// }
 
 
 //switch case
