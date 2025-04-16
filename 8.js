05d1267bba32c051ff418e10f26f44a222f4afdc
Arun Kumar Giri <atulgiri649@gmail.com>
2025-04-16 09:50:22 +0530

    Added JavaScript files and React FrontEnd project

--- 8.js
+++ 8.js
@@ -78,3 +78,40 @@ my name is newton
 
 
 console.log("Arun")
+
+
+
+
+
+
+
+let char1="abc"// char1->"abc"->removed from here
+let char2="def"// char 2 ->"def"
+let char3=char2// char 3 ->"def"
+char1=char3// char 4 ->"def"
+
+
+let arr1=[1,2,3];
+let arr2=arr1;
+arr2[2]=4;
+console.log(arr2)
+//arr1->[1,2,3]<- arr2
+//301->[1,2,3];
+//arr->301,arr2->301
+//arr1->[1,2,3]
+
+
+
+
+
+
+let hello1="hello1";
+let hello2="hello2";
+
+console.log(hello1)
+for(let i=0; i<100000000; i++){
+
+}
+
+//Problem With this code is that its taking time 
+console.log(hello2)
